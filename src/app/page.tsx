99661d4df46cf8bb4d21712bc98b596e5747235b
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-12-15 00:01:17 +0600

    feat: add new page component using ReactFileUploaderUI and update its package version.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
-
 import ReactFileUploaderUI from "react-file-upload-ui";
 
 export default function Home() {
   return (
-    <div className="container min-h-screen flex justify-center items-center">
-      <div>
-        <ReactFileUploaderUI />
+    <div className="container mx-auto min-h-screen flex justify-center items-center">
+      <div className="max-w-md w-full">
+        <ReactFileUploaderUI className="border-red-500"/>
       </div>
     </div>
   );
